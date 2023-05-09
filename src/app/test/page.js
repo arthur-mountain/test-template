62f081eb61ff782b5014637205292e573f1521f7
aaron <aaron.tai@coolbe.com.tw>
2023-05-09 22:31:22 +0800

    Test image

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,12 +1,21 @@
 "use client";
 import Button from '@mui/material/Button';
-// import Image from 'next/image';
+import Image from 'next/image';
 // import styles from "./test.module.scss";
 
 export default function TestPage() {
   return (
     <main className="flex">
       test
+      <div>
+        <Image
+          src="next.svg"
+          width="150"
+          height="150"
+          alt="test"
+          style={{ backgroundColor: '#fff' }}
+        />
+      </div>
       <div>
         <Button variant="text">Text</Button>
       </div>
